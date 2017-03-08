14903d0da6f310c948cac255964d06a5dbcce2f5
Zhu Tengfei <tengfei.zhu@hpe.com>
2017-03-08 14:52:55 +0800

    7. Temporary UI state

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,18 +1,33 @@
-import {Template} from "meteor/templating";
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import "./body.html";
 
 import "./task.js";
 
-import {Tasks} from "../api/tasks.js"
+import { Tasks } from "../api/tasks.js"
+
+Template.body.onCreated(function(){
+  this.state = new ReactiveDict();
+});
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({}, {sort: {createdAt: -1}});
+    const instance = Template.instance();
+
+    if (instance.state.get('hideCompleted')) {
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    } else {
+      return Tasks.find({}, { sort: { createdAt: -1 } });
+    }
   },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  }
 });
 
 Template.body.events({
+  // add new task
   'submit .new-task'(event) {
     // prevent default browser form submit
     event.preventDefault();
@@ -29,5 +44,10 @@ Template.body.events({
 
     //clear form
     target.text.value = '';
+  },
+
+  // hide completed tasks
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
