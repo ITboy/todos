e1f57848cd85a6c87dbc886fd7afa7ef43f920cc
Zhu Tengfei <tengfei.zhu@hpe.com>
2017-03-08 12:08:19 +0800

    4. Forms and events

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,26 @@ import {Tasks} from "../api/tasks.js"
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    return Tasks.find({}, {sort: {createdAt: -1}});
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // prevent default browser form submit
+    event.preventDefault();
+
+    // get value from event
+    const target = event.target;
+    const text = target.text.value;
+
+    // insert a task into collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    //clear form
+    target.text.value = '';
   },
 });
